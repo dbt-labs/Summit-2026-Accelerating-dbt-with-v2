e61eed8c0a84aff68ccb8d1fd34420c39ef49b48
marvin geisler <marvin.geisler@dbtlabsworkshop.com>
2026-09-16 19:33:46 +0000

    tasks 1 and 2

--- models/marts/fct_store_daily.sql
+++ models/marts/fct_store_daily.sql
@@ -3,11 +3,11 @@ with orders as (
     select
         order_id,
         store_id,
-        orderedat,
-        sub_total,
+        ordered_at,
+        subtotal,
         tax_paid,
         order_total
-    from {{ ref('stg_jaffle_shop__order') }}
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -15,10 +15,9 @@ stores as (
 
     select
         store_id,
-        store_locaton,
-        taxrate,
-        opened_at,
-        is_open
+        store_location,
+        tax_rate,
+        opened_at
     from {{ ref('stg_jaffle_shop__stores') }}
 
 ),
@@ -32,21 +31,20 @@ daily_rollup as (
         date_trunc('day', orders.ordered_at) as order_date,
 
         count(orders.order_id) as orders_count,
-        sum(coalesce(orders.subtotal)) as daily_subtotal,
+        sum(coalesce(orders.subtotal, 0)) as daily_subtotal,
         sum(orders.tax_paid) as daily_tax_paid,
         sum(orders.order_total) as daily_order_total,
 
         daily_order_total / orders_count as avg_order_total,
 
-        datediff('day', orders.ordered_at, stores.opened_at) as days_since_store_open
+        datediff('day', stores.opened_at, date_trunc('day', orders.ordered_at)) as days_since_store_open
+
 
     from orders
     left join stores
         on orders.store_id = stores.store_id
 
-    group by
-        orders.store_id,
-        order_date
+    group by all
 
 )
 
