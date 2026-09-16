4953891cf07b5806b053781e6869b6180c7762f5
erika dixon <erika.dixon@dbtlabsworkshop.com>
2026-09-16 19:32:55 +0000

    Practices 1&2

--- dbt_v2_vhol_practice_tasks/task_2/task_2_fct_store_daily_buggy.sql
+++ dbt_v2_vhol_practice_tasks/task_2/task_2_fct_store_daily_buggy.sql
@@ -3,11 +3,11 @@ with orders as (
     select
         order_id,
         store_id,
-        orderedat,
-        sub_total,
+        ordered_at,
+        subtotal,
         tax_paid,
         order_total
-    from {{ ref('stg_jaffle_shop__order') }}
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -15,10 +15,9 @@ stores as (
 
     select
         store_id,
-        store_locaton,
-        taxrate,
-        opened_at,
-        is_open
+        store_location,
+        tax_rate,
+        opened_at
     from {{ ref('stg_jaffle_shop__stores') }}
 
 ),
@@ -29,16 +28,15 @@ daily_rollup as (
         orders.store_id,
         stores.store_location,
         stores.tax_rate,
+        date_trunc('day', stores.opened_At) as opened_at_date,
         date_trunc('day', orders.ordered_at) as order_date,
 
         count(orders.order_id) as orders_count,
-        sum(coalesce(orders.subtotal)) as daily_subtotal,
+        sum(coalesce(orders.subtotal, 0)) as daily_subtotal,
         sum(orders.tax_paid) as daily_tax_paid,
         sum(orders.order_total) as daily_order_total,
 
-        daily_order_total / orders_count as avg_order_total,
-
-        datediff('day', orders.ordered_at, stores.opened_at) as days_since_store_open
+        daily_order_total / orders_count as avg_order_total
 
     from orders
     left join stores
@@ -46,8 +44,17 @@ daily_rollup as (
 
     group by
         orders.store_id,
-        order_date
+        order_date,
+        stores.store_location,
+        stores.tax_rate,
+        stores.opened_at
 
+),
+
+final as (
+    select 
+        datediff('day', order_date, opened_at_date) as days_since_store_open
+    from daily_rollup
 )
 
 select *
