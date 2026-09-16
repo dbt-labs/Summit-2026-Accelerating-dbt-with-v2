c36dede25749de7fc87676e5e999bd3612a735f8
pranita nigam <pranita.nigam@dbtlabsworkshop.com>
2026-09-16 19:32:26 +0000

    dbt-workshop-practice-test

--- models/marts/task_1_fct_orders_buggy.sql
+++ models/marts/task_1_fct_orders_buggy.sql
@@ -8,7 +8,7 @@ with orders as (
         subtotal,
         tax_paid,
         order_total
-    from {{ ref('stg_jaffle_shop__order') }}
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -17,7 +17,7 @@ stores as (
     select
         store_id,
         store_location,
-        taxrate
+        tax_rate
     from {{ ref('stg_jaffle_shop__stores') }}
 
 ),
@@ -32,6 +32,7 @@ order_items as (
 
 ),
 
+
 item_rollup as (
 
     select
@@ -39,7 +40,7 @@ item_rollup as (
         count(distinct order_item_id) as items_count,
         count(product_id) as distinct_products_count
     from order_items
-    group by order_id, product_id
+    group by order_id
 
 ),
 
@@ -62,7 +63,7 @@ joined as (
         item_rollup.distinct_products_count,
 
         orders.subtotal * stores.tax_rate as expected_tax,
-        orders.tax_paid - expected_tax as tax_delta
+        round(orders.tax_paid - expected_tax,2) as tax_delta
 
     from orders
 
@@ -70,7 +71,7 @@ joined as (
         on orders.store_id = stores.store_id
 
     left join item_rollup
-        on order_items.order_id = item_rollup.order_id
+        on orders.order_id = item_rollup.order_id
 
 )
 
