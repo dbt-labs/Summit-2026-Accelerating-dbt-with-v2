0ba50d45fc6ca32fcebb622ca1704b081fd0a994
troy wilkinson <troy.wilkinson@dbtlabsworkshop.com>
2026-09-16 19:39:59 +0000

    done

--- dbt_v2_vhol_practice_tasks/task_2/task_2_fct_store_daily_buggy.sql
+++ dbt_v2_vhol_practice_tasks/task_2/task_2_fct_store_daily_buggy.sql
@@ -3,11 +3,11 @@ with orders as (
     select
         order_id,
         store_id,
-        orderedat,
-        sub_total,
+        ordered_at,
+        subtotal_cents,
         tax_paid,
         order_total
-    from {{ ref('stg_jaffle_shop__order') }}
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -15,10 +15,10 @@ stores as (
 
     select
         store_id,
-        store_locaton,
-        taxrate,
+        store_location,
+        tax_rate,
         opened_at,
-        is_open
+        opened_at <= current_timestamp() as is_open
     from {{ ref('stg_jaffle_shop__stores') }}
 
 ),
@@ -28,11 +28,11 @@ daily_rollup as (
     select
         orders.store_id,
         stores.store_location,
-        stores.tax_rate,
+        stores.tax_rate as tax_rate,
         date_trunc('day', orders.ordered_at) as order_date,
 
         count(orders.order_id) as orders_count,
-        sum(coalesce(orders.subtotal)) as daily_subtotal,
+        sum(coalesce(orders.subtotal_cents,0)) as daily_subtotal,
         sum(orders.tax_paid) as daily_tax_paid,
         sum(orders.order_total) as daily_order_total,
 
@@ -46,7 +46,9 @@ daily_rollup as (
 
     group by
         orders.store_id,
-        order_date
+        stores.store_location,
+        order_date,
+        tax_rate
 
 )
 
