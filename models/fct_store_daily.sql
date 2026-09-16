917d685bca252e5859ab6e806b39fb0250216304
eric skidmore <eric.skidmore@dbtlabsworkshop.com>
2026-09-16 19:33:07 +0000

    task 2 fix

---
+++ models/fct_store_daily.sql
@@ -0,0 +1,45 @@
+with
+    orders as (
+
+        select order_id, store_id, ordered_at, subtotal, tax_paid, order_total
+        from {{ ref("stg_jaffle_shop__orders") }}
+
+    ),
+
+    stores as (
+
+        select store_id, store_location, tax_rate, opened_at
+        from {{ ref("stg_jaffle_shop__stores") }}
+
+    ),
+
+    daily_rollup as (
+
+        select
+            orders.store_id,
+            stores.store_location,
+            stores.tax_rate,
+            date_trunc('day', orders.ordered_at) as order_date,
+
+            count(orders.order_id) as orders_count,
+            sum(coalesce(orders.subtotal, 0)) as daily_subtotal,
+            sum(orders.tax_paid) as daily_tax_paid,
+            sum(orders.order_total) as daily_order_total,
+
+            daily_order_total / orders_count as avg_order_total,
+
+            datediff(
+                'day', orders.ordered_at, stores.opened_at
+            ) as days_since_store_open
+
+        from orders
+        left join stores on orders.store_id = stores.store_id
+
+        group by orders.store_id, stores.store_location, 
+        stores.tax_rate, order_date, days_since_store_open
+
+    )
+
+select *
+from daily_rollup
+order by order_date desc, store_id
