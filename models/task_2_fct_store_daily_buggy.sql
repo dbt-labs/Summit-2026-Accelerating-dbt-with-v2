024ae671d47f524a38f1b30fead8e8da6ae28f1c
craig homan <craig.homan+1789583136@dbtlabsworkshop.com>
2026-09-25 13:52:40 +0000

    clean up before showing team some state

--- models/task_2_fct_store_daily_buggy.sql
+++ models/task_2_fct_store_daily_buggy.sql
@@ -3,11 +3,11 @@ with orders as (
     select
         order_id,
         store_id,
-        orderedat,
-        sub_total,
+        ordered_at,
+        subtotal_cents,
         tax_paid,
         order_total
-    from {{ ref('stg_jaffle_shop__order') }}
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -15,10 +15,10 @@ stores as (
 
     select
         store_id,
-        store_locaton,
-        taxrate,
-        opened_at,
-        is_open
+        store_location,
+        tax_rate,
+        opened_at --,
+        --is_open
     from {{ ref('stg_jaffle_shop__stores') }}
 
 ),
@@ -29,24 +29,23 @@ daily_rollup as (
         orders.store_id,
         stores.store_location,
         stores.tax_rate,
+
         date_trunc('day', orders.ordered_at) as order_date,
 
         count(orders.order_id) as orders_count,
-        sum(coalesce(orders.subtotal)) as daily_subtotal,
+        sum(coalesce(orders.subtotal_cents/100, 0)) as daily_subtotal,
         sum(orders.tax_paid) as daily_tax_paid,
         sum(orders.order_total) as daily_order_total,
 
-        daily_order_total / orders_count as avg_order_total,
+       daily_order_total / nullif(orders_count, 0) as avg_order_total,
 
-        datediff('day', orders.ordered_at, stores.opened_at) as days_since_store_open
+        datediff('day', stores.opened_at, orders.ordered_at) as days_since_store_open
 
     from orders
     left join stores
         on orders.store_id = stores.store_id
 
-    group by
-        orders.store_id,
-        order_date
+    group by all
 
 )
 
